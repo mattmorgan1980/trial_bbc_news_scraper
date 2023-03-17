5a337605221f0971307ebef526f3dcf20b19daa6
Matt Morgan <50718413+mattmorgan1980@users.noreply.github.com>
2023-03-17 16:32:50 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,13 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import requests
+from bs4 import BeautifulSoup
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+url = "https://www.bbc.com/news"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+articles = soup.find_all("h3", class_="gs-c-promo-heading__title")
+
+for article in articles:
+    title = article.text.strip()
+    scraperwiki.sqlite.save(unique_keys=["title"], data={"title": title})
